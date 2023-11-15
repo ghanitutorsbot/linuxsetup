92bd17e29c8f5813ef3ed4f4667b9f9221a82977
learnsoftwares <60375240+learnsoftwares@users.noreply.github.com>
2023-11-15 16:59:36 +0530

    Create jenkinsinstallation.sh

---
+++ new/jenkinsinstallation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download Jenkins GPG key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Add Jenkins repository to Apt sources
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update apt packages
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install -y jenkins
